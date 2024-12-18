5f30ae2ec4844dbeb7f72178db5f62f02eb24eeb
Josh Mitchell <joshuami@users.noreply.github.com>
2024-12-18 11:03:11 -0800

    Remove login and update Footer to have license.

---
+++ web/sites/default/files/php/trash/Drupal__node__NodeStorageTrash67631b88625a2/aeNZF5ZJCPFpf6NtZoODfPXZdFIXsfYgVzP8e53BDqA.php
@@ -0,0 +1,12 @@
+<?php
+
+use Drupal\trash\TrashStorageTrait;
+
+/**
+ * Provides a custom storage class for trash-enabled entity types.
+ */
+class Drupal__node__NodeStorageTrash67631b88625a2 extends \Drupal\node\NodeStorage {
+
+  use TrashStorageTrait;
+
+}
