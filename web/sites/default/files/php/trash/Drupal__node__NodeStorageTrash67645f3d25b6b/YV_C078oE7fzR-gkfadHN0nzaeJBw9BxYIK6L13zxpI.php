785fc2ef37e82b1263811916e5d11b186e2387db
Josh Mitchell <joshuami@users.noreply.github.com>
2024-12-19 10:01:40 -0800

    Update the homepage.

---
+++ web/sites/default/files/php/trash/Drupal__node__NodeStorageTrash67645f3d25b6b/YV_C078oE7fzR-gkfadHN0nzaeJBw9BxYIK6L13zxpI.php
@@ -0,0 +1,12 @@
+<?php
+
+use Drupal\trash\TrashStorageTrait;
+
+/**
+ * Provides a custom storage class for trash-enabled entity types.
+ */
+class Drupal__node__NodeStorageTrash67645f3d25b6b extends \Drupal\node\NodeStorage {
+
+  use TrashStorageTrait;
+
+}
