d8d209e385ba5c8eb5f126b61e7db452d5c4b625
Josh Mitchell <joshuami@users.noreply.github.com>
2025-02-26 11:05:19 -0800

    Correct spacing in footer.

---
+++ web/sites/default/files/php/twig/67bf654a30bf9_block--system-branding-bl_3SbY-WaAGC2v4L0p1U3T0IgO2/4z2h3ZSlR9ThIlkwW_8p94CN5iItGn-0LoN8A0pFkFg.php
@@ -0,0 +1,217 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\CoreExtension;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+use Twig\TemplateWrapper;
+
+/* themes/custom/xtra/templates/block/block--system-branding-block.html.twig */
+class __TwigTemplate_948b5763229aa10df3784023b15e5bc6 extends Template
+{
+    private Source $source;
+    /**
+     * @var array<string, Template>
+     */
+    private array $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->extensions[SandboxExtension::class];
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = []): iterable
+    {
+        $macros = $this->macros;
+        // line 15
+        yield from $this->loadTemplate("themes/custom/xtra/templates/block/block--system-branding-block.html.twig", "themes/custom/xtra/templates/block/block--system-branding-block.html.twig", 15, "369205278")->unwrap()->yield(CoreExtension::merge($context, ["block_utility_classes" => ["block--system-branding"]]));
+        yield from [];
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName(): string
+    {
+        return "themes/custom/xtra/templates/block/block--system-branding-block.html.twig";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function isTraitable(): bool
+    {
+        return false;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo(): array
+    {
+        return array (  44 => 15,);
+    }
+
+    public function getSourceContext(): Source
+    {
+        return new Source("", "themes/custom/xtra/templates/block/block--system-branding-block.html.twig", "/var/www/html/web/themes/custom/xtra/templates/block/block--system-branding-block.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = ["embed" => 15];
+        static $filters = [];
+        static $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['embed'],
+                [],
+                [],
+                $this->source
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
+
+
+/* themes/custom/xtra/templates/block/block--system-branding-block.html.twig */
+class __TwigTemplate_948b5763229aa10df3784023b15e5bc6___369205278 extends Template
+{
+    private Source $source;
+    /**
+     * @var array<string, Template>
+     */
+    private array $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->blocks = [
+            'block_content' => [$this, 'block_block_content'],
+        ];
+        $this->sandbox = $this->extensions[SandboxExtension::class];
+        $this->checkSecurity();
+    }
+
+    protected function doGetParent(array $context): bool|string|Template|TemplateWrapper
+    {
+        return "xtra:block";
+    }
+
+    protected function doDisplay(array $context, array $blocks = []): iterable
+    {
+        $macros = $this->macros;
+        $this->parent = $this->loadTemplate("xtra:block", "themes/custom/xtra/templates/block/block--system-branding-block.html.twig", 15);
+        yield from $this->parent->unwrap()->yield($context, array_merge($this->blocks, $blocks));
+        $this->env->getExtension('\Drupal\Core\Template\TwigExtension')
+            ->checkDeprecations($context, ["site_name", "site_slogan"]);    }
+
+    // line 21
+    /**
+     * @return iterable<null|scalar|\Stringable>
+     */
+    public function block_block_content(array $context, array $blocks = []): iterable
+    {
+        $macros = $this->macros;
+        // line 22
+        yield "    ";
+        yield from $this->loadTemplate("xtra:navbar-brand", "themes/custom/xtra/templates/block/block--system-branding-block.html.twig", 22)->unwrap()->yield(CoreExtension::merge($context, ["text" =>         // line 23
+($context["site_name"] ?? null), "slogan" =>         // line 24
+($context["site_slogan"] ?? null), "path" => $this->extensions['Drupal\Core\Template\TwigExtension']->getPath("<front>")]));
+        // line 27
+        yield "  ";
+        yield from [];
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName(): string
+    {
+        return "themes/custom/xtra/templates/block/block--system-branding-block.html.twig";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function isTraitable(): bool
+    {
+        return false;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo(): array
+    {
+        return array (  156 => 27,  154 => 24,  153 => 23,  151 => 22,  144 => 21,  44 => 15,);
+    }
+
+    public function getSourceContext(): Source
+    {
+        return new Source("", "themes/custom/xtra/templates/block/block--system-branding-block.html.twig", "/var/www/html/web/themes/custom/xtra/templates/block/block--system-branding-block.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = ["extends" => 15, "include" => 22];
+        static $filters = [];
+        static $functions = ["path" => 25];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['extends', 'include'],
+                [],
+                ['path'],
+                $this->source
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
