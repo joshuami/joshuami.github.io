3a45db8c1e326f1ad99adfebcf311d109859021a
Josh Mitchell <joshuami@users.noreply.github.com>
2025-03-19 17:46:11 -0700

    Some Thoughts About How AI Could Reshape CMS Platforms

---
+++ web/sites/default/files/php/twig/67db6c804c330_views-view-grid.html.twig_ZIt9gPnaFEhYDDrqoE1VJh0sj/etjp9WYRztCmJ0qo7sg4Mdy7p_GhyG2msSw-orL3J9I.php
@@ -0,0 +1,97 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\CoreExtension;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+use Twig\TemplateWrapper;
+
+/* themes/contrib/radix/src/kits/radix_starterkit/templates/views/views-view-grid.html.twig */
+class __TwigTemplate_f124f62320d3c3ba47c387824c2454a7 extends Template
+{
+    private Source $source;
+    /**
+     * @var array<string, Template>
+     */
+    private array $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->extensions[SandboxExtension::class];
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = []): iterable
+    {
+        $macros = $this->macros;
+        // line 25
+        yield from $this->loadTemplate("radix:views-view--grid", "themes/contrib/radix/src/kits/radix_starterkit/templates/views/views-view-grid.html.twig", 25)->unwrap()->yield($context);
+        yield from [];
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName(): string
+    {
+        return "themes/contrib/radix/src/kits/radix_starterkit/templates/views/views-view-grid.html.twig";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo(): array
+    {
+        return array (  44 => 25,);
+    }
+
+    public function getSourceContext(): Source
+    {
+        return new Source("", "themes/contrib/radix/src/kits/radix_starterkit/templates/views/views-view-grid.html.twig", "/var/www/html/web/themes/contrib/radix/src/kits/radix_starterkit/templates/views/views-view-grid.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = ["include" => 25];
+        static $filters = [];
+        static $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['include'],
+                [],
+                [],
+                $this->source
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
