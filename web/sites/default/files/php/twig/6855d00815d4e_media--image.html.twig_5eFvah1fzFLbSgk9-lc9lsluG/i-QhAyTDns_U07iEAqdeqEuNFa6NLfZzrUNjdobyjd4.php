24b28f1546b1ccd24c0ad14877e66bbaee868970
Josh Mitchell <joshuami@users.noreply.github.com>
2025-06-20 17:30:25 -0700

    Add note from phenaproxima that any version of Drupal supporting recipes will do.

---
+++ web/sites/default/files/php/twig/6855d00815d4e_media--image.html.twig_5eFvah1fzFLbSgk9-lc9lsluG/i-QhAyTDns_U07iEAqdeqEuNFa6NLfZzrUNjdobyjd4.php
@@ -0,0 +1,172 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\CoreExtension;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+use Twig\TemplateWrapper;
+
+/* themes/custom/xtra/templates/media/media--image.html.twig */
+class __TwigTemplate_04517900ae98e656aeb232f919a3348f extends Template
+{
+    private Source $source;
+    /**
+     * @var array<string, Template>
+     */
+    private array $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->extensions[SandboxExtension::class];
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = []): iterable
+    {
+        $macros = $this->macros;
+        // line 2
+        $context["classes"] = ["media", ("media--type-" . \Drupal\Component\Utility\Html::getClass(CoreExtension::getAttribute($this->env, $this->source,         // line 4
+($context["media"] ?? null), "bundle", [], "method", false, false, true, 4))), (((($tmp =  !CoreExtension::getAttribute($this->env, $this->source,         // line 5
+($context["media"] ?? null), "isPublished", [], "method", false, false, true, 5)) && $tmp instanceof Markup ? (string) $tmp : $tmp)) ? ("media--unpublished") : ("")), (((($tmp =         // line 6
+($context["view_mode"] ?? null)) && $tmp instanceof Markup ? (string) $tmp : $tmp)) ? (("media--view-mode-" . \Drupal\Component\Utility\Html::getClass(($context["view_mode"] ?? null)))) : (""))];
+        // line 9
+        yield "<div";
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, CoreExtension::getAttribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [($context["classes"] ?? null)], "method", false, false, true, 9), "html", null, true);
+        yield ">
+  ";
+        // line 10
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, CoreExtension::getAttribute($this->env, $this->source, ($context["title_suffix"] ?? null), "contextual_links", [], "any", false, false, true, 10), "html", null, true);
+        yield "
+  ";
+        // line 11
+        if ((($tmp = ($context["content"] ?? null)) && $tmp instanceof Markup ? (string) $tmp : $tmp)) {
+            // line 12
+            yield "    ";
+            // line 13
+            if (((((($context["view_mode"] ?? null) != "release") && (($context["view_mode"] ?? null) != "image_gallery_thumbnail")) && (($context["view_mode"] ?? null) != "output_url")) && (CoreExtension::getAttribute($this->env, $this->source, ($context["media"] ?? null), "id", [], "any", false, false, true, 13) != 21))) {
+                // line 14
+                yield "      ";
+                if ((((($_v0 = ($context["attributes"] ?? null)) && is_array($_v0) || $_v0 instanceof ArrayAccess && in_array($_v0::class, CoreExtension::ARRAY_LIKE_CLASSES, true) ? ($_v0["data-fullsize"] ?? null) : CoreExtension::getAttribute($this->env, $this->source, ($context["attributes"] ?? null), "data-fullsize", [], "array", false, false, true, 14)) == "1") || (Twig\Extension\CoreExtension::trim(Twig\Extension\CoreExtension::striptags($this->extensions['Drupal\Core\Template\TwigExtension']->renderVar(CoreExtension::getAttribute($this->env, $this->source, ($context["content"] ?? null), "field_link_to_fullsize", [], "any", false, false, true, 14)))) == "On"))) {
+                    // line 15
+                    yield "        <a title=\"View full sized image.\" class=\"position-relative img-fullsize d-block border border-2 border-primary p-1\" href=\"";
+                    yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\Core\Template\TwigExtension']->getPath("entity.media.canonical", ["media" => CoreExtension::getAttribute($this->env, $this->source, ($context["media"] ?? null), "id", [], "any", false, false, true, 15)]), "html", null, true);
+                    yield "\">
+          <div class=\"fs-icon\"><i class=\"fal fa-arrow-up-right-and-arrow-down-left-from-center\"></i></div>
+          ";
+                    // line 17
+                    yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\Core\Template\TwigExtension']->withoutFilter(($context["content"] ?? null), "field_link_to_fullsize"), "html", null, true);
+                    yield "
+        </a>
+      ";
+                } else {
+                    // line 20
+                    yield "        ";
+                    yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\Core\Template\TwigExtension']->withoutFilter(($context["content"] ?? null), "field_link_to_fullsize"), "html", null, true);
+                    yield "
+      ";
+                }
+                // line 22
+                yield "      ";
+                if ((($tmp = CoreExtension::getAttribute($this->env, $this->source, ($context["user"] ?? null), "hasPermission", ["edit any image media"], "method", false, false, true, 22)) && $tmp instanceof Markup ? (string) $tmp : $tmp)) {
+                    // line 23
+                    yield "        <div class=\"media-edit small m-1\">
+          <a href=\"";
+                    // line 24
+                    yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\Core\Template\TwigExtension']->getPath("entity.media.canonical", ["media" => CoreExtension::getAttribute($this->env, $this->source, ($context["media"] ?? null), "id", [], "any", false, false, true, 24)]), "html", null, true);
+                    yield "/edit\" target=\"_blank\">Edit</a>
+        </div>
+      ";
+                }
+                // line 27
+                yield "    ";
+            } else {
+                // line 28
+                yield "      ";
+                yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\Core\Template\TwigExtension']->withoutFilter(($context["content"] ?? null), "field_link_to_fullsize"), "html", null, true);
+                yield "
+    ";
+            }
+            // line 30
+            yield "  ";
+        }
+        // line 31
+        yield "</div>
+";
+        $this->env->getExtension('\Drupal\Core\Template\TwigExtension')
+            ->checkDeprecations($context, ["media", "view_mode", "attributes", "title_suffix", "content", "user"]);        yield from [];
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName(): string
+    {
+        return "themes/custom/xtra/templates/media/media--image.html.twig";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function isTraitable(): bool
+    {
+        return false;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo(): array
+    {
+        return array (  109 => 31,  106 => 30,  100 => 28,  97 => 27,  91 => 24,  88 => 23,  85 => 22,  79 => 20,  73 => 17,  67 => 15,  64 => 14,  62 => 13,  60 => 12,  58 => 11,  54 => 10,  49 => 9,  47 => 6,  46 => 5,  45 => 4,  44 => 2,);
+    }
+
+    public function getSourceContext(): Source
+    {
+        return new Source("", "themes/custom/xtra/templates/media/media--image.html.twig", "/var/www/html/web/themes/custom/xtra/templates/media/media--image.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = ["set" => 2, "if" => 11];
+        static $filters = ["clean_class" => 4, "escape" => 9, "trim" => 14, "striptags" => 14, "render" => 14, "without" => 17];
+        static $functions = ["path" => 15];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set', 'if'],
+                ['clean_class', 'escape', 'trim', 'striptags', 'render', 'without'],
+                ['path'],
+                $this->source
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
---
+++ web/sites/default/files/php/twig/6855d00815d4e_xtra:page-content_hrcJkfOPITG17-edN3eI5dmU9/o9gONkvEQGfIpCJEzihfsYU8iL3R2vlo88VzM1aVLtc.php
@@ -0,0 +1,233 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\CoreExtension;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+use Twig\TemplateWrapper;
+
+/* xtra:page-content */
+class __TwigTemplate_d0639a4789eae904128aea730848ff87 extends Template
+{
+    private Source $source;
+    /**
+     * @var array<string, Template>
+     */
+    private array $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+            'page_inner_content' => [$this, 'block_page_inner_content'],
+        ];
+        $this->sandbox = $this->extensions[SandboxExtension::class];
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = []): iterable
+    {
+        $macros = $this->macros;
+        // line 1
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar($this->extensions['Drupal\Core\Template\TwigExtension']->attachLibrary("core/components.xtra--page-content"));
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar($this->extensions['Drupal\Core\Template\ComponentsTwigExtension']->addAdditionalContext($context, "xtra:page-content"));
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar($this->extensions['Drupal\Core\Template\ComponentsTwigExtension']->validateProps($context, "xtra:page-content"));
+        // line 2
+        $context["page_main_classes"] = Twig\Extension\CoreExtension::merge([], (((($tmp =         // line 3
+($context["page_main_utility_classes"] ?? null)) && $tmp instanceof Markup ? (string) $tmp : $tmp)) ? (($context["page_main_utility_classes"] ?? null)) : ([])));
+        // line 5
+        yield "
+";
+        // line 7
+        $context["header_classes"] = ["page__header", "container-fluid"];
+        // line 13
+        yield "
+<main";
+        // line 14
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, CoreExtension::getAttribute($this->env, $this->source, ($context["content_attributes"] ?? null), "addClass", [($context["page_main_classes"] ?? null)], "method", false, false, true, 14), "html", null, true);
+        yield " id=\"main-content\">
+  ";
+        // line 18
+        yield "  ";
+        $context["preheader_render"] = $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar(CoreExtension::getAttribute($this->env, $this->source, ($context["page"] ?? null), "preheader", [], "any", false, false, true, 18));
+        // line 19
+        yield "  ";
+        if ((($tmp = Twig\Extension\CoreExtension::trim(($context["preheader_render"] ?? null))) && $tmp instanceof Markup ? (string) $tmp : $tmp)) {
+            // line 20
+            yield "    <div class=\"page__preheader\">
+      <div class=\"container-fluid bg-faded-gray\">
+        <div class=\"container\">
+          ";
+            // line 23
+            yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, ($context["preheader_render"] ?? null), "html", null, true);
+            yield "
+        </div>
+      </div>
+    </div>
+  ";
+        }
+        // line 28
+        yield "  ";
+        $context["header_render"] = $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar(CoreExtension::getAttribute($this->env, $this->source, ($context["page"] ?? null), "header", [], "any", false, false, true, 28));
+        // line 29
+        yield "  ";
+        if ((($tmp = Twig\Extension\CoreExtension::trim(($context["header_render"] ?? null))) && $tmp instanceof Markup ? (string) $tmp : $tmp)) {
+            // line 30
+            yield "    <header";
+            yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, CoreExtension::getAttribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [($context["header_classes"] ?? null)], "method", false, false, true, 30), "html", null, true);
+            yield "\">
+      <div class=\"container\">
+        <div class=\"row align-items-center\">
+          <div class=\"col py-4\">
+            ";
+            // line 34
+            yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, ($context["header_render"] ?? null), "html", null, true);
+            yield "
+          </div>
+          ";
+            // line 36
+            $context["featured_image_render"] = $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar(CoreExtension::getAttribute($this->env, $this->source, ($context["page"] ?? null), "featured_image", [], "any", false, false, true, 36));
+            // line 37
+            yield "          ";
+            if ((($tmp = Twig\Extension\CoreExtension::trim(($context["featured_image_render"] ?? null))) && $tmp instanceof Markup ? (string) $tmp : $tmp)) {
+                // line 38
+                yield "          <div class=\"col col-md-6\">
+            ";
+                // line 39
+                yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, ($context["featured_image_render"] ?? null), "html", null, true);
+                yield "
+          </div>
+          ";
+            }
+            // line 42
+            yield "        </div>
+      </div>
+    </header>
+  ";
+        }
+        // line 46
+        yield "  <div class=\"container\">
+    <div class=\"row\">
+      ";
+        // line 48
+        if ((($tmp = CoreExtension::getAttribute($this->env, $this->source, ($context["page"] ?? null), "content", [], "any", false, false, true, 48)) && $tmp instanceof Markup ? (string) $tmp : $tmp)) {
+            // line 49
+            yield "        <div class=\"page__content col\">
+            ";
+            // line 50
+            yield from $this->unwrap()->yieldBlock('page_inner_content', $context, $blocks);
+            // line 53
+            yield "        </div>
+      ";
+        }
+        // line 55
+        yield "      ";
+        // line 57
+        yield "      ";
+        $context["sidebar_render"] = $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar(CoreExtension::getAttribute($this->env, $this->source, ($context["page"] ?? null), "sidebar", [], "any", false, false, true, 57));
+        // line 58
+        yield "      ";
+        if ((($tmp = Twig\Extension\CoreExtension::trim(($context["sidebar_render"] ?? null))) && $tmp instanceof Markup ? (string) $tmp : $tmp)) {
+            // line 59
+            yield "        <div class=\"page__sidebar col-md-12 col-lg-4\">
+          ";
+            // line 60
+            yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, ($context["sidebar_render"] ?? null), "html", null, true);
+            yield "
+        </div>
+      ";
+        }
+        // line 63
+        yield "    </div>
+  </div>
+</main>";
+        $this->env->getExtension('\Drupal\Core\Template\TwigExtension')
+            ->checkDeprecations($context, ["page_main_utility_classes", "content_attributes", "page", "attributes"]);        yield from [];
+    }
+
+    // line 50
+    /**
+     * @return iterable<null|scalar|\Stringable>
+     */
+    public function block_page_inner_content(array $context, array $blocks = []): iterable
+    {
+        $macros = $this->macros;
+        // line 51
+        yield "              ";
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, CoreExtension::getAttribute($this->env, $this->source, ($context["page"] ?? null), "content", [], "any", false, false, true, 51), "html", null, true);
+        yield "
+            ";
+        yield from [];
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName(): string
+    {
+        return "xtra:page-content";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function isTraitable(): bool
+    {
+        return false;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo(): array
+    {
+        return array (  169 => 51,  162 => 50,  154 => 63,  148 => 60,  145 => 59,  142 => 58,  139 => 57,  137 => 55,  133 => 53,  131 => 50,  128 => 49,  126 => 48,  122 => 46,  116 => 42,  110 => 39,  107 => 38,  104 => 37,  102 => 36,  97 => 34,  89 => 30,  86 => 29,  83 => 28,  75 => 23,  70 => 20,  67 => 19,  64 => 18,  60 => 14,  57 => 13,  55 => 7,  52 => 5,  50 => 3,  49 => 2,  45 => 1,);
+    }
+
+    public function getSourceContext(): Source
+    {
+        return new Source("", "xtra:page-content", "themes/custom/xtra/components/page-content/page-content.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = ["set" => 2, "if" => 19, "block" => 50];
+        static $filters = ["merge" => 3, "escape" => 14, "render" => 18, "trim" => 19];
+        static $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set', 'if', 'block'],
+                ['merge', 'escape', 'render', 'trim'],
+                [],
+                $this->source
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
